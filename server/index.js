f6a063422504e5622f276977169f6bdd47f317a8
anmolpal2001 <anmolpal2001@gmail.com>
2024-01-02 02:29:12 +0530

    little changes

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   next();
 });
 
