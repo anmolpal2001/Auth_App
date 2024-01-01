32dee4be40e05a30614f9e078bdcc1d2473d42e1
anmolpal2001 <anmolpal2001@gmail.com>
2024-01-02 01:47:26 +0530

    server-side update

--- server/index.js
+++ server/index.js
@@ -6,11 +6,16 @@ import userRoutes from "./routes/user.route.js"
 import authRoutes from "./routes/auth.route.js"
 import firstMiddleware from "./middlewares/user.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true,
+}));
 
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/auth", authRoutes);
