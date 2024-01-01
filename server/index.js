1b274e141fbc5f891f8a20955aae701c87aedde0
anmolpal2001 <anmolpal2001@gmail.com>
2024-01-02 01:50:26 +0530

    index.js file updated

--- server/index.js
+++ server/index.js
@@ -12,10 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  origin: "http://localhost:3000",
-  credentials: true,
-}));
+app.use(cors());
 
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/auth", authRoutes);
