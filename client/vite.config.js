f51645edea04d20297d535c00d5f27bb57b73720
Anmol Pal <85279423+anmolpal2001@users.noreply.github.com>
2024-01-02 14:03:42 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // const base_URL = 'https://auth-app-server-tuhe.onrender.com'
-const base_URL = 'http://localhost:3000'
+// const base_URL = 'http://localhost:3000'
+const base_URL = 'https://auth-app-client-3cbu.onrender.com'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
