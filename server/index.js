de5f4093a1a33cdd998f32dffe5de9be69c34c45
Anmol Pal <85279423+anmolpal2001@users.noreply.github.com>
2024-01-03 22:25:33 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,10 @@ app.use(cookieParser());
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin","*");
+  res.setHeader("Access-Control-Allow-Methods","GET,POST,DELETE");
+  res.setHeader("Access-Control-Allow-Headers","Content-Type");
+  // res.setHeader("Content-Type", "application/json");
   next();
 });
 
